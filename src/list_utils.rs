4d82f8448853c39f5a2678c6232a7298858b8e0f
grdvsng <grdvsng@gmail.com>
2020-01-28 15:24:52 +0300

    update

--- src/list_utils.rs
+++ src/list_utils.rs
@@ -15,20 +15,17 @@ impl ToString for Nil
 }
 
 
-enum Item<T>
+enum Item<'a, 'b, 'c, T>
     where
-        T: ToString + fmt::Debug,
+        T: ToString + fmt::Debug + std::cmp::PartialEq,
 {
-    First (T, Nil, Nil),
-    Top   (T, Nil, Box<Item<T>>),
-    Midle (T, Box<Item<T>>, Box<Item<T>>),
-    Last  (T, Box<Item<T>>, Nil),
+    Cout(&'a T, &'b Item<'a, 'b, 'c, T>, &'c Item<'a, 'b, 'c, T>),
     Null,
 }
 
-impl<T> Item<T>
+impl<'a, 'b, 'c, T> Item<'a, 'b, 'c, T>
     where
-        T: ToString + fmt::Debug,
+        T: ToString + fmt::Debug + std::cmp::PartialEq,
 {
     fn to_string(&self) -> String
     {
@@ -36,20 +33,26 @@ impl<T> Item<T>
 
         match &self 
         {
-            Item::First (v, ..) => value = v,
-            Item::Top   (v, ..) => value = v,
-            Item::Midle (v, ..) => value = v,
-            Item::Last  (v, ..) => value = v,
+            Item::Cout (v, ..) => value = v,
             _                   => return String::from("Nil"),
         }
 
         return String::from(value.to_string());
     }
+
+    fn get_value(&self) -> Result<&T, String>
+    {
+        match &self 
+        {
+            Item::Cout (v, ..) => Ok(v),
+            _                  => Err(self.to_string()),
+        }
+    }
 }
 
-impl<T> fmt::Display for Item<T> 
+impl<'a, 'b, 'c, T> fmt::Display for Item<'a, 'b, 'c, T>
     where
-        T: ToString + fmt::Debug,
+        T: ToString + fmt::Debug + std::cmp::PartialEq,
 {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result
     {
@@ -57,9 +60,9 @@ impl<T> fmt::Display for Item<T>
     }
 }
 
-impl<T> fmt::Debug for Item<T> 
+impl<'a, 'b, 'c, T> fmt::Debug for Item<'a, 'b, 'c, T> 
     where
-        T: ToString + fmt::Debug,
+        T: ToString + fmt::Debug + std::cmp::PartialEq,
 {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result
     {
@@ -68,28 +71,29 @@ impl<T> fmt::Debug for Item<T>
 }
 
 
-struct List<T>
+struct List<'a, 'b, 'c, T>
     where
-        T: ToString + fmt::Debug,
+        T: ToString + fmt::Debug + std::cmp::PartialEq,
 {
     length: isize,
-    cursor:  Item<T>,
+    cursor: Item<'a, 'b, 'c, T>,
 }
 
-trait ListMethods<T>
+trait ListMethods<'a, 'b, 'c, T>
     where
-        T: ToString + fmt::Debug,
+        T: ToString + fmt::Debug + std::cmp::PartialEq,
 {
-    // Constructor
-    fn new()                     -> List<T>;
-    // rem Last element from List and return his ref
-    fn pop(&mut self)            -> Result<&Item<T>, String>;
-    // rem First element from List and return his ref
-    fn top(&mut self)            -> Result<&Item<T>, String>;
-    // return Index of value in List or -1
-    //fn index_of<T>(&self, value: T) -> isize;
-    // Return value bye index or null if index bigger then length
-    fn get(&self, index: isize)  -> Result<&Item<T>, String>;
+    fn new()                                                             -> List<'a, 'b, 'c, T>;
+    fn pop(&mut self)                                                    -> Result<&Item<T>, String>;
+    fn top(&mut self)                                                    -> Result<&Item<T>, String>;
+    fn _is_value_eq(&self, value: Option<&'static T>, item: &Item<T>)    -> bool;
+    fn index_of(&self, value: &'static T)                                -> isize;
+    fn get(&self, index: isize)                                          -> Result<&Item<T>, String>;
+    fn _get(&self, by_val: Option<&'static T>, by_index:  Option<isize>) -> Result<(&Item<T>, isize), String>;
+    fn _get_index_by_value(&self, value: &'static T)                     -> Result<(&Item<T>, isize), String>;
+    fn _get_by_index(&self, index: isize)                                -> Result<&Item<T>, String>;
+    fn get_validator(&self, index: isize)                                -> bool;
+    fn append(&mut self, value: &'a T);
     // Clear list
     /*fn clear(&mut self);
     // Set new value on index
@@ -103,11 +107,11 @@ trait ListMethods<T>
 */
 }
 
-impl<T> ListMethods<T> for List<T>
+impl<'a, 'b, 'c, T> ListMethods<'a, 'b, 'c, T> for List<'a, 'b, 'c, T>
     where
-        T: ToString + fmt::Debug,
+        T: ToString + fmt::Debug + std::cmp::PartialEq,
 {
-    fn new() -> List<T>
+    fn new() -> List<'a, 'b, 'c, T>
     {
         let nil: Item<T> = Item::Null;
         
@@ -119,7 +123,7 @@ impl<T> ListMethods<T> for List<T>
 
     fn pop(&mut self) -> Result<&Item<T>, String>
     {
-        if (self.length > 1) 
+        if self.length > 1
         {
             return self.get(self.length-1);
         } else {
@@ -129,36 +133,123 @@ impl<T> ListMethods<T> for List<T>
 
     fn top(&mut self) ->Result<&Item<T>, String>
     {
-        if (self.length > 1) 
+        if self.length > 1
         {
             return self.get(0);
         } else {
             return Ok(&self.cursor);
         }
     }
+    
+    fn index_of(&self, value: &'static T) -> isize
+    {
+        match self._get_index_by_value(&value)
+        {
+            Ok(result) => return result.1,
+            Err(_)     => -1 as isize,
+        }
+    }
+
+    fn get_validator(&self, index: isize) -> bool
+    {
+        if index > self.length-1 || self.length == 0
+        {
+            return false;
+        } else { return true; }
+    }
+    
+    fn _is_value_eq(&self, value: Option<&'static T>, item: &Item<T>) -> bool
+    {
+        match value
+        {
+            Some(data) =>
+                match &item
+                {
+                    Item::Cout (v, ..) => v == &data,
+                    _                  => false,
+                },
+            _ => false,
+        }
+    }
+
+    fn _get(&self, by_val: Option<&'static T>, by_index:  Option<isize>) -> Result<(&Item<T>, isize), String>
+    {
+        match by_val
+        {
+            Some(v) => 
+                match self._get_index_by_value(v)
+                {
+                    Ok(v)  => Ok(v),
+                    Err(e) => Err(e),
+                }
+            _       =>
+                match by_index
+                {
+                    Some(i) =>   
+                        match self.get(i)
+                        {
+                            Ok(v)  => Ok((v, i)),
+                            Err(e) => Err(e),
+                        }
+                        ,
+                    _       => Err(String::from("Need use byVal or byIndex parametr!")),
+                }
+        }
+    }
 
     fn get(&self, index: isize) -> Result<&Item<T>, String>
     {
-        let nil: Item<T> = Item::Null;
-        let mut next     = &self.cursor;
+        if !self.get_validator(index) 
+        { 
+            return Result::Err(format!("Index '{}' out of List length('{}').", index, self.length)); 
+        } else {
+            return self._get_by_index(index);
+        }
+    }
 
-        if (index > self.length-1 || self.length == 0)
+    fn _get_index_by_value(&self, value: &'static T) -> Result<(&Item<T>, isize), String>
+    {
+        let mut next = &self.cursor;
+        
+        for i in 0..self.length
         {
-            return Result::Err(format!("Index '{}' out of List length('{}').", index, self.length));
-        } 
+            if self._is_value_eq(Some(&value), &next) { return Ok((&next, i)); }
 
+            match &next
+            {
+                Item::Cout(_, _, n) => next = n,
+                _                   => return Result::Err(String::from("Undefined error!")),
+            }
+        }
+
+        return Err(String::from(format!("index of vallue: '{:?}', not found", value)));
+    }
+
+    fn _get_by_index(&self, index: isize) -> Result<&Item<T>, String>
+    {
+        let mut next = &self.cursor;
+        
         for i in 0..self.length
         {
             if i == index { break; }
             
             match &next
             {
-                Item::Top(s, p, n)   => next = &*n,
-                Item::Midle(s, p, n) => next = &*n,
+                Item::Cout(_, _, n)   => next = &*n,
                 _                    => return Result::Err(String::from("Undefined error!")),
             }
         }
 
         return Result::Ok(next);
+    }
+
+    fn append(&mut self, value: &'a T)
+    {
+        if self.length == 0
+        {
+            self.cursor = Item::Cout(value, &Item::Null, &Item::Null);
+        }
+
+        self.length += 1;
     }
 }
